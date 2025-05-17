460bd4ef549e120ec473da9c444e5110e2c82955
toprogrammer <toprogrammer@yandex.ru>
2025-05-17 20:49:34 +0300

    modify log formatter

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,16 @@
 import logging
+import os
 
-# Simple logger setup
+# Create logs directory if it doesn't exist
+os.makedirs('./logs', exist_ok=True)
+
+# Enhanced logger setup with more detailed formatting
 logger = logging.getLogger('logger')
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('logs.txt')
-formatter = logging.Formatter('%(asctime)s - %(message)s')
+file_handler = logging.FileHandler('./logs/logs.txt')
+formatter = logging.Formatter(
+    fmt="%(asctime)s [%(levelname)s] [%(name)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
