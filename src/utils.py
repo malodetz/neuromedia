e4ad3b50a3b7870b91b74a2e1b292797ffcd91b2
DerekBum <alextruewestern@gmail.com>
2025-05-17 15:18:16 +0300

    core: configure logging
    Update `utils` and use it for logging in core.

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,14 @@
 import logging
-import os
 
-# Enhanced logger setup with more detailed formatting
-logger = logging.getLogger('logger')
-logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler('run.log')
-formatter = logging.Formatter(
-    fmt="%(asctime)s [%(levelname)s] [%(name)s] %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S"
-)
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+def get_logger(name: str):
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        file_handler = logging.FileHandler('run.log')
+        formatter = logging.Formatter(
+            "%(asctime)s [%(levelname)s] [%(name)s] %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+    return logger
